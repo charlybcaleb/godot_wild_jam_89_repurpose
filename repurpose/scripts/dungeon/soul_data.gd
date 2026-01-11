6897ee417d7e9ea69596fda4bd2b356dc755e48a
charlybcaleb <matteumayo@gmail.com>
2026-01-11 15:52:46 -0800

    fucking souls

--- repurpose/scripts/dungeon/soul_data.gd
+++ repurpose/scripts/dungeon/soul_data.gd
@@ -2,6 +2,7 @@ class_name SoulData
 extends Resource
 
 @export var name: String
-@export var enemy_data: EnemyData
+## must be looked up by path + name
+@export var enemy_data_name: String
 @export var mp_cost:= 3
 @export var description: String
--- repurpose/scripts/npc/soul.gd
+++ repurpose/scripts/npc/soul.gd
@@ -1,7 +1,40 @@
-extends Node2D
+extends CharacterBody2D
 
 @export var soul_data: SoulData
+var soul_state:= SoulState.IDLE
+
+@export var mouse_follow_speed:= 50.0
 var grid: AStarGrid2D
 var current_cell: Vector2i
+var cur_pt: int
+var target_cell: Vector2i
+var move_pts: Array
 
 enum SoulState { IDLE, CHANNEL, WORK, HELD }
+
+func _ready(): 
+	GameMan.register_enemy(self)
+	add_to_group("soul")
+	set_physics_process(false)
+
+# called by GameMan
+func setup(_grid: AStarGrid2D):
+	grid = _grid
+	current_cell = GameMan.pos_to_cell(global_position)
+	target_cell = current_cell
+
+func _process(delta: float) -> void:
+	if soul_state == SoulState.HELD:
+		var mouse_pos = Vector2.ZERO
+		mouse_pos = get_global_mouse_position()
+		global_position = global_position.lerp(mouse_pos, delta * mouse_follow_speed)
+
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	if event is InputEventMouseButton  \
+	and event.button_index == MOUSE_BUTTON_LEFT \
+	and event.is_pressed():
+		if soul_state != SoulState.HELD:
+			soul_state = SoulState.HELD
+		else:
+			#todo: check and see if over valid tile.
+			soul_state = SoulState.IDLE
