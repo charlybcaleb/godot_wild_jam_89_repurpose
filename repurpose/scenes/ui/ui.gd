dc8ffd46d932350f431167b6a6c23770baedd990
charlybcaleb <matteumayo@gmail.com>
2026-01-14 17:31:53 -0800

    everything workking just need to add connection to last room

--- repurpose/scenes/ui/ui.gd
+++ repurpose/scenes/ui/ui.gd
@@ -6,10 +6,12 @@ var enemy_popup_target: Node2D
 var show_popup = false
 var popup_offset:= Vector2(6,-72)
 #map
-@export var map_rooms: Array[Control] # 17 rooms total
+@export var map_rooms: Array[Node] # 17 rooms total
 
 # FIXME: this is terrible code and just made for jam it's so bad fuck myu life
 func link_map_rooms():
+	map_rooms.clear()
+	map_rooms = $Map.get_children()
 	# room 0 column 0
 	# if first room, make link to each in next column (mr[1,2,3])
 	var link_count = randi_range(3,3)
@@ -22,6 +24,11 @@ func link_map_rooms():
 	# rooms 1-3 / column 1 
 	for mr in map_rooms:
 		mr.debug_label.text = str(map_rooms.find(mr))
+	
+	turn_off_next_col_rooms(1)
+	turn_off_next_col_rooms(4)
+	turn_off_next_col_rooms(7)
+	turn_off_next_col_rooms(10)
 	setup_map_room_column(1)
 	setup_map_room_column(4)
 	setup_map_room_column(7)
@@ -29,14 +36,15 @@ func link_map_rooms():
 
 
 func setup_map_room_column(col_start_index: int):
-
-	turn_off_next_col_rooms(col_start_index)
 	await get_tree().process_frame
 	link_col_rooms_to_next_col(col_start_index, 2)
 	await get_tree().process_frame
 	#prune_rooms_with_no_fwd_links(col_start_index)
 	await get_tree().process_frame
 	link_adj_rooms_in_column(col_start_index)
+	await get_tree().create_timer(0.8).timeout
+	prune_rooms_with_no_links()
+
 
 func turn_off_next_col_rooms(col_start_index: int):
 	var turn_off_a_room = randi_range(0,3)
@@ -46,7 +54,7 @@ func turn_off_next_col_rooms(col_start_index: int):
 		map_rooms[random_mr_in_next_column].set_disabled(true)
 
 func prune_rooms_with_no_fwd_links(col_start_index: int):
-	var mr_to_prune: Array[Control]
+	var mr_to_prune: Array[Node]
 	for i in map_rooms.size()-1:
 		if i > col_start_index and i < col_start_index+3:
 			var has_forward_link = false
@@ -63,6 +71,16 @@ func prune_rooms_with_no_fwd_links(col_start_index: int):
 					print("link pruned")
 		map_rooms[map_rooms.find(mr)].set_disabled(true)
 
+func prune_rooms_with_no_links():
+	var mr_to_prune: Array[Node]
+	for mr in map_rooms:
+		var has_link = true
+		if !map_rooms[map_rooms.find(mr)].map_room_links.size() > 0:
+				has_link = false
+		if has_link == false:
+			mr_to_prune.append(map_rooms[map_rooms.find(mr)])
+	for mr in mr_to_prune:
+		map_rooms[map_rooms.find(mr)].set_disabled(true)
 
 func link_adj_rooms_in_column(col_start_index: int):
 	var mr_index = col_start_index
@@ -82,7 +100,7 @@ func link_col_rooms_to_next_col(col_start_index: int, forward_links: int):
 	var mr_index = col_start_index
 	var unpicked_from_rooms:= [col_start_index,col_start_index+1,col_start_index+2]
 	# pick 1-2 random rooms to link forward
-	for i in range(forward_links):
+	for i in (forward_links):
 		var from_room: int
 		var to_room: int
 		from_room = unpicked_from_rooms[randi_range(0,unpicked_from_rooms.size()-1)]
@@ -103,10 +121,9 @@ func link_col_rooms_to_next_col(col_start_index: int, forward_links: int):
 					to_room = col_start_index+3
 				else:
 					to_room = col_start_index+4
-		else:
-			unpicked_from_rooms.pop_at(unpicked_from_rooms.find(from_room))
-			var new_link = MapRoomLink.new(map_rooms[from_room], map_rooms[to_room])
-			map_rooms[mr_index].add_link(new_link)
+		unpicked_from_rooms.pop_at(unpicked_from_rooms.find(from_room))
+		var new_link = MapRoomLink.new(map_rooms[from_room], map_rooms[to_room])
+		map_rooms[mr_index].add_link(new_link)
 		mr_index+=1
 
 #func link_map_rooms_in_column(col_start_index)
