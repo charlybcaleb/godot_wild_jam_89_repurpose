e437bfdd0bce8b05fe47ba8b4d57b11ab5f3f34a
charlybcaleb <matteumayo@gmail.com>
2026-01-14 16:54:29 -0800

    bacup before change ui scene

--- repurpose/scenes/ui/map_room.gd
+++ repurpose/scenes/ui/map_room.gd
@@ -10,6 +10,7 @@ var room_data: RoomData
 var center_offset = Vector2(32,32)
 var is_disabled = false
 var map_room_links: Array[MapRoomLink] 
+@export var debug_label: Label
 
 func set_icon_sprite(sprite: Texture2D):
 	icon_sprite.texture = sprite
@@ -20,8 +21,14 @@ func add_link(map_room_link: MapRoomLink):
 	lines[map_room_links.size()-1].clear_points()
 	lines[map_room_links.size()-1].add_point(global_position+center_offset)
 	lines[map_room_links.size()-1].add_point(map_room_link.to.global_position+center_offset)
-	print("line set with point count " + str(lines[map_room_links.size()-1].get_point_count()))
+
+func remove_link(map_room_link: MapRoomLink):
+	if map_room_links.has(map_room_link):
+		map_room_links.pop_at(map_room_links.find(map_room_link))
 
 func set_disabled(e= true):
 	is_disabled = e
 	hide()
+
+func get_links() -> Array[MapRoomLink]:
+	return map_room_links
--- repurpose/scenes/ui/ui.gd
+++ repurpose/scenes/ui/ui.gd
@@ -20,15 +20,49 @@ func link_map_rooms():
 			var new_link = MapRoomLink.new(from_room, to_room)
 			map_rooms[0].add_link(new_link)
 	# rooms 1-3 / column 1 
-	turn_off_next_col_rooms(1)
-	link_adj_rooms_in_column(1)
-	link_col_rooms_to_next_col(1, randi_range(1,2))
+	for mr in map_rooms:
+		mr.debug_label.text = str(map_rooms.find(mr))
+	setup_map_room_column(1)
+	setup_map_room_column(4)
+	setup_map_room_column(7)
+	setup_map_room_column(10)
+
+
+func setup_map_room_column(col_start_index: int):
+
+	turn_off_next_col_rooms(col_start_index)
+	await get_tree().process_frame
+	link_col_rooms_to_next_col(col_start_index, 2)
+	await get_tree().process_frame
+	#prune_rooms_with_no_fwd_links(col_start_index)
+	await get_tree().process_frame
+	link_adj_rooms_in_column(col_start_index)
 
 func turn_off_next_col_rooms(col_start_index: int):
-	var turn_off_a_room = randi_range(0,1)
-	if turn_off_a_room == 1:
+	var turn_off_a_room = randi_range(0,3)
+	if turn_off_a_room >= 1:
 		var random_mr_in_next_column = randi_range(col_start_index+3,col_start_index+5)
+		print(random_mr_in_next_column)
 		map_rooms[random_mr_in_next_column].set_disabled(true)
+
+func prune_rooms_with_no_fwd_links(col_start_index: int):
+	var mr_to_prune: Array[Control]
+	for i in map_rooms.size()-1:
+		if i > col_start_index and i < col_start_index+3:
+			var has_forward_link = false
+			for link in map_rooms[i].map_room_links:
+				if link.from.global_position.x < link.to.global_position.x:
+					has_forward_link = true
+			if has_forward_link == false:
+				mr_to_prune.append(map_rooms[i])
+	for mr in mr_to_prune:
+		for r in map_rooms:
+			for l in map_rooms[map_rooms.find(r)].get_links():
+				if l.to == mr:
+					r.remove_link(l)
+					print("link pruned")
+		map_rooms[map_rooms.find(mr)].set_disabled(true)
+
 
 func link_adj_rooms_in_column(col_start_index: int):
 	var mr_index = col_start_index
@@ -39,7 +73,7 @@ func link_adj_rooms_in_column(col_start_index: int):
 			if from_room.is_disabled: return # don't start link from disabled room
 			var to_room = map_rooms[mr_index+1]
 			if to_room.is_disabled and mr_index != col_start_index+2:
-				to_room = map_rooms[to_room+1]
+				to_room = map_rooms[map_rooms.find(to_room)+1]
 			var new_link = MapRoomLink.new(from_room, to_room)
 			map_rooms[mr_index].add_link(new_link)
 		mr_index += 1
@@ -54,12 +88,26 @@ func link_col_rooms_to_next_col(col_start_index: int, forward_links: int):
 		from_room = unpicked_from_rooms[randi_range(0,unpicked_from_rooms.size()-1)]
 		to_room = randi_range(col_start_index+3,col_start_index+5)
 		if map_rooms[to_room].is_disabled:
-			forward_links+=1
-			continue
+			# if disabled, go to other room in column
+			if to_room == col_start_index+3:
+				to_room = randi_range(col_start_index+4, col_start_index+5)
+			elif to_room == col_start_index+4:
+				var coin_flip = randi_range(0,1)
+				if coin_flip == 0:
+					to_room = col_start_index+3
+				else:
+					to_room = col_start_index+5
+			elif to_room == col_start_index+5:
+				var coin_flip = randi_range(0,1)
+				if coin_flip == 0:
+					to_room = col_start_index+3
+				else:
+					to_room = col_start_index+4
 		else:
 			unpicked_from_rooms.pop_at(unpicked_from_rooms.find(from_room))
 			var new_link = MapRoomLink.new(map_rooms[from_room], map_rooms[to_room])
 			map_rooms[mr_index].add_link(new_link)
+		mr_index+=1
 
 #func link_map_rooms_in_column(col_start_index)
 
